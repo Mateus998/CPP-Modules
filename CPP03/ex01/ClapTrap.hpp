8e8d6a5d6895b57f5baf25e04f6cd375ec61ec06
mateus_vm <mateus@vm.com>
2025-12-04 14:23:02 +0000

    changepc

---
+++ CPP03/ex01/ClapTrap.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mateferr <mateferr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/03 18:25:48 by mateferr          #+#    #+#             */
+/*   Updated: 2025/12/03 18:40:40 by mateferr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+
+class ClapTrap{
+    private:
+        std::string _name;
+        int _hit;
+        int _energy;
+        int _attack;
+    
+    public:
+        ClapTrap(void);
+        ClapTrap(const std::string _name);
+        ClapTrap(const ClapTrap& copy);
+        ~ClapTrap(void);
+        ClapTrap& operator=(const ClapTrap& copy);
+        
+        void attack(const std::string& target);
+        void takeDamage(unsigned int amount);
+        void beRepaired(unsigned int amount);
+        
+        std::string getName(void) const;
+        int getHit(void) const;
+        int getEnergy(void) const;
+        int getAttack(void) const;
+};
+
+#endif
